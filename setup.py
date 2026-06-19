eb2c3509af20dac6eb211485bdde7c48f841527b
gohdeuf <tillvollmert@outlook.de>
2026-06-19 16:47:40 +0200

    Test for GitHub actions to work

--- setup.py
+++ setup.py
@@ -4,40 +4,40 @@ setup(
     name="Bindoj",
     options={
         "build_apps": {
-            "platforms": ["win_amd64"],
-            
-            # Hier wird der Name der ausführbaren Datei festgelegt (erzeugt Bindoj.exe)
-            "gui_apps": {
-                "Bindoj": "main.py",
-            },
-            
-            # WICHTIG: Diese Module müssen für die Windows-.exe geladen werden
-            "include_modules": [
-                "panda3d",
-                "direct"
-            ],
-            
-            "plugins": [
+        "platforms": ["win_amd64"],
+
+        # TEMPORÄR zum Debuggen: zeigt ein CMD-Fenster mit allen Ausgaben/Tracebacks
+        "console_apps": {
+            "Bindoj": "main.py",
+        },
+        # "gui_apps": {
+        #     "Bindoj": "main.py",
+        # },
+
+        "include_modules": [
+        "panda3d",
+        "direct"
+        ],
+        "plugins": [
                 "pandagl",
-                "p3openal_audio",
-            ],
-            
-            "requirements_path": "requirements.txt",
-            
-            "include_patterns": [
-                "world/**/*",
-                "models/**/*",
-                "entities/**/*",
-                "ui/**/*",
-                "**/*.png",
-                "**/*.jpg",
-                "**/*.mp3",
-                "**/*.wav",
-                "**/*.ursina"
-            ],
-            
-            # Verhindert Fehler mit unpassenden, vorkompilierten Binärdateien
-            "use_optimized_wheels": False
-        }
+            "p3openal_audio",
+        ],
+        "requirements_path": "requirements.txt",
+        "include_patterns": [
+            "world/**/*",
+            "models/**/*",
+            "entities/**/*",
+            "ui/**/*",
+            "**/*.png",
+            "**/*.jpg",
+            "**/*.mp3",
+            "**/*.wav",
+            "**/*.glb",
+            "**/*.ursinamesh",
+            "**/*.ttf",
+            "**/*.otf",
+        ],
+        "use_optimized_wheels": False
     }
+}
 )
