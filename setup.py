1ae58fca450212c8cc3526a28916b953812c2216
gohdeuf <tillvollmert@outlook.de>
2026-06-20 20:05:17 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,40 +4,47 @@ setup(
     name="Bindoj",
     options={
         "build_apps": {
-        "platforms": ["win_amd64"],
+            "platforms": ["win_amd64"],
 
-        # TEMPORÄR zum Debuggen: zeigt ein CMD-Fenster mit allen Ausgaben/Tracebacks
-        "console_apps": {
-            "Bindoj": "main.py",
-        },
-        # "gui_apps": {
-        #     "Bindoj": "main.py",
-        # },
+            "gui_apps": {
+                "Bindoj": "main.py",
+            },
 
-        "include_modules": [
-        "panda3d",
-        "direct"
-        ],
-        "plugins": [
+            # Damit du bei künftigen Abstürzen einen Log siehst statt nichts:
+            "log_filename": "$USER_APPDATA/Bindoj/output.log",
+            "log_append": False,
+
+            "include_modules": {
+                "*": [
+                    "panda3d",
+                    "direct",
+                    "numpy._core._exceptions",
+                ]
+            },
+
+            "plugins": [
                 "pandagl",
-            "p3openal_audio",
-        ],
-        "requirements_path": "requirements.txt",
-        "include_patterns": [
-            "world/**/*",
-            "models/**/*",
-            "entities/**/*",
-            "ui/**/*",
-            "**/*.png",
-            "**/*.jpg",
-            "**/*.mp3",
-            "**/*.wav",
-            "**/*.glb",
-            "**/*.ursinamesh",
-            "**/*.ttf",
-            "**/*.otf",
-        ],
-        "use_optimized_wheels": False
+                "p3openal_audio",
+            ],
+
+            "requirements_path": "requirements.txt",
+
+            "include_patterns": [
+                "world/**/*",
+                "models/**/*",
+                "entities/**/*",
+                "ui/**/*",
+                "**/*.png",
+                "**/*.jpg",
+                "**/*.mp3",
+                "**/*.wav",
+                "**/*.glb",
+                "**/*.ursinamesh",
+                "**/*.ttf",
+                "**/*.otf",
+            ],
+
+            "use_optimized_wheels": False
+        }
     }
-}
 )
